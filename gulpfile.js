a80700763dbf70d5b4ad4b4a144b9054e9c48d38
Mubeen Jassani <mubeen.jasani@gmail.com>
2019-08-15 11:24:16 +0200

    polyfill included in bundle instead of index.html

--- gulpfile.js
+++ gulpfile.js
@@ -17,8 +17,8 @@ const config = {
         dist: './dist/'
     },
     localServer: {
-        port: 8001,
-        url: 'http://localhost:8001/'
+        port: 8002,
+        url: 'http://localhost:8002/'
     }
 };
 
--- src/entry.js
+++ src/entry.js
@@ -1,5 +1,6 @@
 
 // entry.js
+import '@webcomponents/webcomponentsjs/webcomponents-bundle';
 import './assets/components/custom-number-field/custom-number-field';
 import './assets/components/custom-select-field/custom-select-field';
 import './assets/components/util/json-dispatcher';
